59ea4dbf6e3957049ccbe49aef0516715d281b5c
Guest User <Guest@Epicodus-8E.local>
2017-11-03 11:09:26 -0700

    make pizza constructor and calculate prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,48 @@
 //back-end
 
 // pizza object constructor
+function Pizza(size, toppings) {
+  this.size = 0;
+  this.toppings = 0;
+}
 
-//properties for sizes
-//properties for toppings
+// pizza.size
+// pizza.toppings
+// pizza.cost = pizza.size + pizza.toppings
+
+//if size = small, 8
+//if size = medium, 10
+//if size = large, 12
+
+//if toppingsArray includes anchovies, 3
+//if toppingsArray includes bacon, 4
+//if toppingsArray includes cheese, 2
 
 //prototype method for pizza cost depending on selections
-
-//function for calculating
-
-
+Pizza.prototype.calculate = function() {
+  return this.size + this.toppings;
+}
 
 
 //front-end
 $(document).ready(function() {
+  debugger;
+  $("#pizza-order").submit(function(event) {
+    event.preventDefault();
+    var toppingsArray = [];
+    var size = $("#size").val();
 
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppingsSelected = $(this).val();
+      toppingsArray.push(" " + toppingsSelected);
+    }); //toppings checkbox checking
+
+    alert("you ordered a " + size + toppingsArray + " pizza!")
+
+    // pizza.size =
+    // pizza.toppings =
+
+    //parseInt values?
+
+  }); //pizza-order submitted
 }); //document ready
